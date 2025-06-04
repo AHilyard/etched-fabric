6dbc9be5493e02b6921110fc38ec4045707c2d9e
ThatzOkay <noduhgamingxd@gmail.com>
2025-06-05 00:41:10 +0200

    upstream etched: Fixed label data being removed

--- src/main/java/gg/moonflower/etched/core/Etched.java
+++ src/main/java/gg/moonflower/etched/core/Etched.java
@@ -8,6 +8,8 @@ import gg.moonflower.etched.common.sound.download.SoundCloudSource;
 import gg.moonflower.etched.core.fabric.EtchedConfig;
 import gg.moonflower.etched.core.registry.*;
 import net.minecraft.core.cauldron.CauldronInteraction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
@@ -50,10 +52,17 @@ public class Etched {
         CauldronInteraction.WATER.put(EtchedItems.MUSIC_LABEL.get(), CauldronInteraction.DYED_ITEM);
         CauldronInteraction.WATER.put(EtchedItems.COMPLEX_MUSIC_LABEL.get(), (state, level, pos, player, hand, stack) -> {
             if (!level.isClientSide()) {
-                stack.removeTagKey("Label");
                 ItemStack newStack = new ItemStack(EtchedItems.MUSIC_LABEL.get());
                 newStack.setCount(stack.getCount());
-                newStack.setTag(stack.getTag());
+                if (stack.hasTag()) {
+                    CompoundTag tag = stack.getTag().copy();
+                    if (tag.contains("Label", Tag.TAG_COMPOUND)) {
+                        CompoundTag label = tag.getCompound("Label");
+                        label.remove("PrimaryColor");
+                        label.remove("SecondaryColor");
+                    }
+                    newStack.setTag(tag);
+                }
 
                 player.setItemInHand(hand, newStack);
                 player.awardStat(Stats.CLEAN_ARMOR);
